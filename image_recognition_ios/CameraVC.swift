86883e49fa4fd8ab66ca7a57485080c684f0d037
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-10-26 12:56:06 -0400

    shit. Follow me on twitter '@mothurfucker' fuck

---
+++ image_recognition_ios/CameraVC.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  image_recognition_ios
+//
+//  Created by Farhan Rahman on 10/25/17.
+//  Copyright © 2017 Farhan Rahman. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class CameraVC: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    let captureSession = AVCaptureSession()
+    var previewLayer:CALayer!
+    
+    var captureDevice:AVCaptureDevice!
+    
+    var takePhoto = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareCamera()
+    }
+    
+    
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        if let availableDevices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .back).devices {
+            captureDevice = availableDevices.first
+            beginSession()
+        }
+        
+    }
+    
+    func beginSession () {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(captureDeviceInput)
+            
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            
+            //Send the preview layer to the UIView. 
+            
+            
+            self.previewLayer = previewLayer
+            
+            self.view.layer.addSublayer(self.previewLayer)
+            self.previewLayer.frame = self.view.layer.frame
+            captureSession.startRunning()
+            
+            let dataOutput = AVCaptureVideoDataOutput()
+            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString):NSNumber(value:kCVPixelFormatType_32BGRA)]
+            
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            
+            if captureSession.canAddOutput(dataOutput) {
+                captureSession.addOutput(dataOutput)
+            }
+            
+            captureSession.commitConfiguration()
+            
+            
+            let queue = DispatchQueue(label: "com.brianadvent.captureQueue")
+            dataOutput.setSampleBufferDelegate(self, queue: queue)
+            
+            
+            
+        }
+        
+    }
+    
+
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        
+        if takePhoto {
+            takePhoto = false
+            
+            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
+                
+                let tensorflowVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! TensorflowVC
+                
+                tensorflowVC.takenPhoto = image
+                
+                DispatchQueue.main.async {
+                    self.present(tensorflowVC, animated: true, completion: {
+                        self.stopCaptureSession()
+                    })
+                    
+                }
+            }
+            
+            
+        }
+
+    }
+    
+    
+    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+            
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    func stopCaptureSession () {
+        self.captureSession.stopRunning()
+        
+        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
+            for input in inputs {
+                self.captureSession.removeInput(input)
+            }
+        }
+        
+    }
+    
+
+    
+}
+
---
+++ image_recognition_ios/TensorflowVC.swift
@@ -0,0 +1,35 @@
+//
+//  TensorflowVC.swift
+//  image_recognition_ios
+//
+//  Created by Farhan Rahman on 10/26/17.
+//  Copyright © 2017 Farhan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class TensorflowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
